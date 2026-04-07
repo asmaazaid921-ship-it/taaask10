e563780ab20ae7f79a64ca425aa18fc73a49b8cc
asmaa zaid <asmaazaid921@gmail.com>
2026-04-07 21:49:44 +0200

    Add Migrations

---
+++ taaask10/Migrations/20260403004139_test22.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace taaask10.Migrations
+{
+    /// <inheritdoc />
+    public partial class test22 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    customerid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "varchar(80)", unicode: false, maxLength: 80, nullable: false),
+                    email = table.Column<int>(type: "int", unicode: false, maxLength: 80, nullable: false),
+                    criditcard = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    sales = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.customerid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    product = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    quantity = table.Column<int>(type: "int", nullable: false),
+                    price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    sales = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.product);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Stores",
+                columns: table => new
+                {
+                    storeid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    sales = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stores", x => x.storeid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sales",
+                columns: table => new
+                {
+                    salesid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    productid = table.Column<int>(type: "int", nullable: false),
+                    customerid = table.Column<int>(type: "int", nullable: false),
+                    customeri = table.Column<int>(type: "int", nullable: false),
+                    storeid = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sales", x => x.salesid);
+                    table.ForeignKey(
+                        name: "FK_Sales_Customers_customeri",
+                        column: x => x.customeri,
+                        principalTable: "Customers",
+                        principalColumn: "customerid",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Sales_Products_productid",
+                        column: x => x.productid,
+                        principalTable: "Products",
+                        principalColumn: "product",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Sales_Stores_storeid",
+                        column: x => x.storeid,
+                        principalTable: "Stores",
+                        principalColumn: "storeid",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sales_customeri",
+                table: "Sales",
+                column: "customeri");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sales_productid",
+                table: "Sales",
+                column: "productid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sales_storeid",
+                table: "Sales",
+                column: "storeid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sales");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Stores");
+        }
+    }
+}
